4d31c0ed1ec06b7df34d6a40866d24155a784485
Nathan <nathan.johnson999@aggiemail.usu.edu>
2019-11-29 23:13:47 -0700

    tried to fix julia but having problems with git

--- src/julia.py
+++ src/julia.py
@@ -1,15 +1,19 @@
 # Julia Set Visualizer
-import Gradient
+from Fractal import Fractal
 
-def getColorFromGradient(z):
+
+class Julia:
+
+    def __init__(self, config):
+        self.config = config
+        self.iterations = config['iterations']
+
     """Return the index of the color of the current pixel within the Julia set
     in the gradient array"""
-    # c is the Julia Constant; varying this value can yield interesting images
-    c = complex(-1.0, 0.0)
-
-    gradientRange = 78
-    for i in range(gradientRange):
-        z = z * z + c
-        if abs(z) > 2:
-            return Gradient.getColor(i)
-    return Gradient.getColor(Gradient.length() - 1)
+    def count(self, count):
+        param = self.config['param']
+        for i in range(self.iterations):
+            count = count * count + param
+            if abs(count) > 2:
+                return i
+        return self.iterations - 1
